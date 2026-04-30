f5ef488e3d01d383faa28f9fb5a3ca624c13474e
mafe <mfernandarocosta@gmail.com>
2026-04-30 11:06:51 -0300

    :sparkles: feat: adiciona políticas de autorização para os modelos no AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,11 +5,28 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use App\Models\Auditoria;
+use App\Models\BemMaterial;
+use App\Models\Coleta;
+use App\Models\Curadoria;
+use App\Policies\AuditoriaPolicy;
+use App\Policies\BemMaterialPolicy;
+use App\Policies\ColetaPolicy;
+use App\Policies\CuradoriaPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
+
+    protected $policies = [
+        Coleta::class      => ColetaPolicy::class,
+        BemMaterial::class => BemMaterialPolicy::class,
+        Curadoria::class   => CuradoriaPolicy::class,
+        Auditoria::class   => AuditoriaPolicy::class,
+    ];
+
     /**
      * Register any application services.
      */
@@ -24,6 +41,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+        $this->registerPolicies();
     }
 
     /**
