18c17f8e270c37d725afb5d06a4d776bf4ad197f
mafe <mfernandarocosta@gmail.com>
2026-05-02 19:11:02 -0300

    :sparkles: feat: ajusta a formatação das rotas de autenticação e adiciona rotas para o recurso Coleta

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,19 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\ColetaController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
-    Route::post('/login',  [AuthController::class, 'login']);
+    Route::post('/login', [AuthController::class, 'login']);
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
-        Route::get('/me',      [AuthController::class, 'me']);
+        Route::get('/me', [AuthController::class, 'me']);
     });
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Coletas
+    Route::apiResource('coletas', ColetaController::class);
 });
