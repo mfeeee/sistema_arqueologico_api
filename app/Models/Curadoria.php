aed133bf4dae839a2b0424bcf01b612fc5cea545
mafe <mfernandarocosta@gmail.com>
2026-05-04 10:32:59 -0300

    :sparkles: feat: adiciona uso do HasFactory no modelo Curadoria

--- app/Models/Curadoria.php
+++ app/Models/Curadoria.php
@@ -5,13 +5,14 @@ namespace App\Models;
 use App\Enums\AcaoResultanteCuradoria;
 use App\Enums\StatusCuradoria;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Curadoria extends Model
 {
-    use HasUuids;
+    use HasFactory, HasUuids;
 
     protected $table = 'curadorias';
 
