702d8c5a0af81ebd25f1ebb44b0f073abb0d6ea2
mafe <mfernandarocosta@gmail.com>
2026-05-04 10:50:08 -0300

    :sparkles: feat: cria o controlador AuditoriaController com método para listar auditorias

---
+++ app/Http/Controllers/AuditoriaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Auditoria;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AuditoriaController extends Controller
+{
+    public function index(Request $request): JsonResponse
+    {
+        $this->authorize('viewAny', Auditoria::class);
+
+        $query = Auditoria::with('usuario')
+            ->orderByDesc('data_hora');
+
+        if ($request->filled('entidade_tipo')) {
+            $query->where('entidade_tipo', $request->entidade_tipo);
+        }
+
+        if ($request->filled('usuario_id')) {
+            $query->where('usuario_id', $request->usuario_id);
+        }
+
+        return response()->json($query->paginate(50));
+    }
+}
