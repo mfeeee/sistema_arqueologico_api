f13d773790580ed6d3ca2b894fac21d5454579f7
mafe <mfernandarocosta@gmail.com>
2026-05-03 23:28:31 -0300

    :sparkles: feat: adiciona rotas para Bens Materiais com método de busca próxima e recurso API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\BemMaterialController;
 use App\Http\Controllers\ColetaController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ Route::prefix('auth')->group(function () {
 Route::middleware('auth:sanctum')->group(function () {
     // Coletas
     Route::apiResource('coletas', ColetaController::class);
+
+    // Bens Materiais
+    Route::get('bens-materiais/nearby', [BemMaterialController::class, 'nearby']);
+    Route::apiResource('bens-materiais', BemMaterialController::class);
+
 });
