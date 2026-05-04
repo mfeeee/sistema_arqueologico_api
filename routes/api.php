24e737004a81612dcb502f7e9c9a716c6665a1f5
mafe <mfernandarocosta@gmail.com>
2026-05-04 11:32:58 -0300

    :sparkles: feat: atualiza os namespaces dos controladores para refletir a estrutura de diretórios

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
-use App\Http\Controllers\AuditoriaController;
+use App\Http\Controllers\Admin\AuditoriaController;
 use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\BemMaterialController;
-use App\Http\Controllers\ColetaController;
-use App\Http\Controllers\CuradoriaController;
-use App\Http\Controllers\SincronizacaoController;
+use App\Http\Controllers\Mobile\BemMaterialController;
+use App\Http\Controllers\Mobile\ColetaController;
+use App\Http\Controllers\Admin\CuradoriaController;
+use App\Http\Controllers\Mobile\SincronizacaoController;
 use Illuminate\Support\Facades\Route;
+
+// -- Auth (Publico)
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
@@ -17,7 +19,9 @@ Route::prefix('auth')->group(function () {
     });
 });
 
-Route::middleware('auth:sanctum')->group(function () {
+
+// -- Mobile 
+Route::prefix('v1/mobile')->middleware('auth:sanctum')->group(function () {
     // Coletas
     Route::apiResource('coletas', ColetaController::class);
 
@@ -27,7 +31,10 @@ Route::middleware('auth:sanctum')->group(function () {
     // Bens Materiais
     Route::get('bens-materiais/nearby', [BemMaterialController::class, 'nearby']);
     Route::apiResource('bens-materiais', BemMaterialController::class);
+});
 
+// -- Admin/Site
+Route::prefix('v1/admin')->middleware(['auth:sanctum', 'role:admin,curador'])->group(function () {
     // Curadorias
     Route::get('curadorias', [CuradoriaController::class, 'index']);
     Route::patch('curadorias/{curadoria}/avaliar', [CuradoriaController::class, 'avaliar']);
