91ef835f3506310562f25414d20528cb6397f210
mafe <mfernandarocosta@gmail.com>
2026-05-04 22:39:03 -0300

    :sparkles: feat: adiciona documentação OpenAPI para a API de Coleta Arqueológica

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * @OA\Info(
+ *     title="Sistema de Coleta Arqueológica API",
+ *     version="1.0.0",
+ *     ...
+ * )
+ * @OA\Server(url="/api", description="Servidor principal")
+ * @OA\SecurityScheme(...)
+ */
 abstract class Controller
 {
     use AuthorizesRequests;
