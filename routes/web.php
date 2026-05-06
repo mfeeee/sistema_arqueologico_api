d096529777bd8640c31b0d57d648804738e99bdf
mafe <mfernandarocosta@gmail.com>
2026-05-06 10:19:52 -0300

    :sparkles: feat: adiciona rota para o Laravel Horizon com middleware de autenticação

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
+use Laravel\Horizon\Horizon;
 
 Route::inertia('/', 'welcome', [
     'canRegister' => Features::enabled(Features::registration()),
@@ -9,6 +10,10 @@ Route::inertia('/', 'welcome', [
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+});
+
+Route::prefix('horizon')->middleware(['auth'])->group(function () {
+    Route::horizon();
 });
 
 require __DIR__.'/settings.php';
