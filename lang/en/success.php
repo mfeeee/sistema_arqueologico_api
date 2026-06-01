19ace82031d9f8806f512421af0a5224cfb212a4
mafe <mfernandarocosta@gmail.com>
2026-06-01 11:32:39 -0300

    feat: adiciona arquivos de tradução em inglês para autenticação, erros, paginação, senhas, recursos, sucesso e validação

---
+++ lang/en/success.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'created'             => ':resource created successfully.',
+    'updated'             => ':resource updated successfully.',
+    'deleted'             => ':resource deleted successfully.',
+    'synced'              => 'Sync request received and queued.',
+    'logout'              => 'Logged out successfully.',
+    'password_reset'      => 'Password reset successfully.',
+    'password_reset_sent' => 'If this email is registered, you will receive instructions shortly.',
+];
