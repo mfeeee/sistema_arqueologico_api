b257ae8b94b775059c8efc49a53c59540d99b03e
mafe <mfernandarocosta@gmail.com>
2026-06-01 17:10:18 -0300

    feat: adiciona URL para redefinição de senha e URL do Flutter no arquivo de configuração

--- config/app.php
+++ config/app.php
@@ -54,7 +54,8 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'flutter_reset_url' => env('FLUTTER_RESET_URL', 'arqueologico://reset-password'),
+    'flutter_reset_url'   => env('FLUTTER_RESET_URL', 'arqueologico://reset-password'),
+    'password_reset_url'  => env('PASSWORD_RESET_URL', 'arqueopi://reset-password'),
 
     /*
     |--------------------------------------------------------------------------
