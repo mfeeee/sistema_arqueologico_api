d6b1ae839aa0f01d4bb4c9352749deb2105357b0
mafe <mfernandarocosta@gmail.com>
2026-06-08 01:15:39 -0300

    feat: implementa o modelo SubmissaoAutor com relacionamentos e campos necessários

---
+++ app/Models/SubmissaoAutor.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SubmissaoAutor extends Model
+{
+    use HasFactory;
+    use HasUuids;
+
+    protected $table = 'submissao_autores';
+
+    public $timestamps = false;
+
+    protected $fillable = ['submissao_id', 'nome_autor', 'ordem'];
+
+    public function submissao(): BelongsTo
+    {
+        return $this->belongsTo(SubmissaoArtigo::class, 'submissao_id');
+    }
+}
