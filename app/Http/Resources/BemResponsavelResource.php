c7e4173818ecd8904254797cfb4f70c906ed200f
mafe <mfernandarocosta@gmail.com>
2026-06-16 13:55:05 -0300

    feat: cria o recurso BemResponsavelResource para estruturar dados de responsáveis

---
+++ app/Http/Resources/BemResponsavelResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BemResponsavelResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'papel' => $this->papel,
+            'user'  => [
+                'id'         => $this->usuario?->id,
+                'nome'       => $this->usuario?->name,
+                'email'      => $this->usuario?->email,
+                'avatar_url' => $this->usuario?->avatar_url,
+            ],
+        ];
+    }
+}
